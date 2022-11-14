9435a6d0a2cbeda89fafa38fe14d2221fb9ccb43
vanhao79 <114144404+haotran79@users.noreply.github.com>
2022-11-14 16:22:41 +0700

    Add files via upload

---
+++ Megaman_copy/src/com/hao/userinterface/GameFrame.java
@@ -0,0 +1,52 @@
+package com.hao.userinterface;
+
+import com.hao.effect.CacheDataLoader;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author David Tran
+ */
+public class GameFrame extends JFrame{
+    
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 500;
+    
+    GamePanel gamePanel;
+
+    public GameFrame(){
+        
+        Toolkit toolkit = this.getToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        this.setBounds((dimension.width - SCREEN_WIDTH)/2, (dimension.height - SCREEN_HEIGHT)/2, SCREEN_WIDTH, SCREEN_HEIGHT);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        try {
+            CacheDataLoader.getInstance().LoadData();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        gamePanel = new GamePanel();
+        add(gamePanel);
+        
+        this.addKeyListener(gamePanel);
+        
+    }
+    
+    public void startGame(){
+        gamePanel.startGame();
+    }
+    
+    public static void main(String[] args) {
+        
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.setVisible(true);
+        gameFrame.startGame();
+        
+    }
+}
